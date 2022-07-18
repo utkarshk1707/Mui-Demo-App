2a15c42cc8394cb178509e92cdc46d7b1ba50214
Utkarsh <utkarsh@Blulabs-Utkarsh.local>
2022-07-18 21:10:12 +0530

    updated Themeing functionality

---
+++ src/Stylesheet.tsx
@@ -0,0 +1,44 @@
+import { createTheme } from "@mui/system";
+import { createMuiTheme } from '@material-ui/core/styles'
+
+
+declare module '@mui/material/styles' {
+    interface BreakpointOverrides {
+        xs: false; // removes the `xs` breakpoint
+        sm: false;
+        md: false;
+        lg: false;
+        xl: false;
+        mobile: true; // adds the `mobile` breakpoint
+        tablet: true;
+        desktop: true;
+    }
+}
+
+
+const StyleSheet = () => {
+    const theme = createTheme({
+        palette: {
+            sbn: {
+                main: '#e3cc91',
+                dark: 'blue',
+                light: 'green',
+            },
+            hal: {
+                main: '#CEE5D0',
+                dark: 'blue',
+                light: 'green',
+            }
+        },
+        breakpoints: {
+            values: {
+                mobile: 300, // small phone
+                tablet: 600, // phone
+                desktop: 900, // small laptop
+            }
+        }
+    });
+}
+
+
+export default StyleSheet;
